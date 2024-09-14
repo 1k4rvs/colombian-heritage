b6452e8c10149d0c6680abbdecd825e14af45126
1k4rvs <pedro.vaquero@gmail.com>
2024-09-14 12:08:51 +0200

    first-changes

--- pages/ShowRoutes.py
+++ pages/ShowRoutes.py
@@ -45,6 +45,7 @@ with left_pane:
                    zoom_start=6, min_zoom=5, max_zoom=16,
                    min_lat=16, max_lat= -1,
                    min_lon=-82, max_lon= -67,
+                   control_scale = True
                    )
 
     # Icon Definition
